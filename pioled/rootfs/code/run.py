5398b847930defc3d216eb28a0b5691c8adfb9d2
Sourcery AI <>
2023-05-18 06:46:50 +0000

    'Refactored by Sourcery'

--- pioled/rootfs/code/run.py
+++ pioled/rootfs/code/run.py
@@ -75,8 +75,8 @@ while True:
 
     # Write four lines of text.
 
-    draw.text((x, top + 0), "IP: " + IP, font=font, fill=255)
-    draw.text((x, top + 8), "CPU load: " + CPU, font=font, fill=255)
+    draw.text((x, top + 0), f"IP: {IP}", font=font, fill=255)
+    draw.text((x, top + 8), f"CPU load: {CPU}", font=font, fill=255)
     draw.text((x, top + 16), MemUsage, font=font, fill=255)
     draw.text((x, top + 25), Disk, font=font, fill=255)
 
